ef304061346e5e2d13f66ed172b324276f2ac8b8
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2024-11-26 16:26:41 +0700

    folder belajar javascript

---
+++ Javascript/object.js
@@ -0,0 +1,7 @@
+let biodata = {
+    nama: "Muhammad Afif Naufal",
+    umur: 20,
+    isMahasiswa: true,
+    universitas: "STT Terpadu Nurul Fikri Depok"
+};
+console.log(biodata.nama, biodata.umur, biodata.isMahasiswa, biodata.universitas);
