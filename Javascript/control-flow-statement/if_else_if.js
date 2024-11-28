9ce81cdaea8fa42e0aaa406dc98e7e52b922d07a
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2024-11-28 09:49:41 +0700

    menambahkan if else if dan ternary operator

---
+++ Javascript/control-flow-statement/if_else_if.js
@@ -0,0 +1,11 @@
+let nilai = 69;
+
+if (nilai >= 90) {
+    console.log("Grade A");
+} else if (nilai >= 80) {
+    console.log("Grade B");
+} else if (nilai >= 70) {
+    console.log("Grade C");
+} else {
+    console.log("Grade D");
+}
---
+++ Javascript/control-flow-statement/ternary_operator.js
@@ -0,0 +1,3 @@
+let umur = 16;
+let hasil = (umur >= 17) ? "Boleh membuat SIM" : "Belum cukup umur";
+console.log(hasil);
